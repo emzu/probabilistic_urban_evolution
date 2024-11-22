78b50f30d8667633d8b15fc4a958604d62f28a21
emzu <65582987+emzu@users.noreply.github.com>
2024-11-22 16:11:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='probabilistic-urban-evolution',  # Replace with your package name
+    name='probabilistic_urban_evolution',  # Replace with your package name
     version='0.1.0',  # Replace with your package version
     packages=find_packages(),  # Automatically find packages
     install_requires=[ 'pandas', 'geopandas', 'numpy', 'seaborn', 'matplotlib', 'plotly', 'hmmlearn', 'scikit-gstat', 'scikit-learn'],
